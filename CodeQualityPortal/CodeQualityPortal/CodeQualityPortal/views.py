79ecb43779bc42b970d45a8961c44f615cd609b4
fatemaoolia <fatemaolia@gmail.com>
2019-02-28 22:52:52 -0500

    choose metric page is completed

--- CodeQualityPortal/CodeQualityPortal/CodeQualityPortal/views.py
+++ CodeQualityPortal/CodeQualityPortal/CodeQualityPortal/views.py
@@ -3,44 +3,70 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template, redirect
-from CodeQualityPortal.forms import SubmitRepositoryForm
+from flask import render_template, redirect, request
+from CodeQualityPortal.forms import SubmitRepositoryForm, ChooseMetricsForm
 from CodeQualityPortal import app
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     """Renders the home page."""
     form = SubmitRepositoryForm()
-    if form.validate_on_submit():
-       return redirect('/contact')
+    if request.method == "POST":
+        if form.validate_on_submit():
+            return redirect('/choose-metric')
 
-    print(form.errors.items())
     return render_template(
         'index.html',
-        title='Home Page',
-        year=datetime.now().year,
         form=form
     )
 
-@app.route('/contact')
-def contact():
+@app.route('/choose-metric', methods=['GET', 'POST'])
+def choose_metric():
     """Renders the contact page."""
+    form = ChooseMetricsForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            metrics = []
+            print(request.form)
+            if 'class_hierarchy_level' in request.form:
+                metrics.append('class_hierarchy_level')
+            if 'no_of_methods_per_class' in request.form:
+                metrics.append('no_of_methods_per_class')
+            if 'cyclomatic_complexity' in request.form:
+                metrics.append('cyclomatic_complexity')
+            if 'coupling_between_objects' in request.form:
+                metrics.append('coupling_between_objects')
+            if 'comments_or_documentation' in request.form:
+                metrics.append('comments_or_documentation')
+            if 'lines_of_code' in request.form:
+                metrics.append('lines_of_code')
+            if 'avg_faults' in request.form:
+                metrics.append('avg_faults')
+            if 'no_of_collaborators_per_file' in request.form:
+                metrics.append('no_of_collaborators_per_file')
+
+            metrics = json.dumps(metrics)
+            print(metrics)
+            return render_template('visualisations.html',
+                                   title='About',
+                                   year=datetime.now().year,
+                                   message=metrics)
+
     return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
+        'choose-metric.html',
+        form=form
     )
 
-@app.route('/about')
-def about():
+@app.route('/visualisations', methods=['GET', 'POST'])
+def visualisations():
     """Renders the about page."""
     return render_template(
-        'about.html',
+        'visualisations.html',
         title='About',
         year=datetime.now().year,
         message='Your application description page.'
