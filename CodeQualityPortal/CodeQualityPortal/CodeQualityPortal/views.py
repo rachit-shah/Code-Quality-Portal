bdc03d68eb65f748c76488b4707afc49de23ad0a
fatemaoolia <fatemaolia@gmail.com>
2019-03-20 16:01:27 -0400

    Getting data using Github APIs

--- CodeQualityPortal/CodeQualityPortal/CodeQualityPortal/views.py
+++ CodeQualityPortal/CodeQualityPortal/CodeQualityPortal/views.py
@@ -8,9 +8,26 @@ from CodeQualityPortal.forms import SubmitRepositoryForm, ChooseMetricsForm
 from CodeQualityPortal import app
 from CodeQualityPortal import sql_db
 import logging
+import requests
+import os
+import base64
 import json
 
 logger = logging.getLogger(__name__)
+
+url_root = "https://api.github.com"
+
+token = "8fede56e8b1ee9ac65d84b4b2b9ccec65c19d7db"
+
+headers = {
+    "content-type": "application/json",
+    "Authorization": "token " + token,
+    "Accept": "application/vnd.github.squirrel-girl-preview+json",
+}
+
+
+def parse_file_content(content, param):
+    pass
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -19,6 +36,33 @@ def index():
     form = SubmitRepositoryForm()
     if request.method == "POST":
         if form.validate_on_submit():
+            repo_url = request.form["repo_url"]
+            data = repo_url.split("/")
+            owner = data[3]
+            repo_name = data[4]
+
+            repo_root_url = os.path.join(url_root, "repos", owner, repo_name, "branches/master")
+            response = requests.get(repo_root_url, headers=headers)
+
+            response = response.json()
+            tree_sha = response["commit"]["sha"]
+            repo_tree_url = os.path.join(url_root, "repos", owner, repo_name, "git/trees", tree_sha+"?recursive=1")
+
+            response = requests.get(repo_tree_url, headers=headers)
+            response = response.json()
+
+            tree = response["tree"]
+
+
+            for x in tree:
+                if ".java" in x["path"]:
+                    response = requests.get(x["url"])
+                    response = response.json()["content"]
+                    content = base64.b64decode(response)
+
+                    parse_file_content(content, x["path"].split("/")[-1])
+
+
             sql_db.mock_database_generator()
             return redirect('/choose-metric')
 
@@ -68,3 +112,6 @@ def visualisations():
         'visualisations.html',
         metrics=metrics
     )
+
+
+
