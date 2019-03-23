9622ea0c359c251d316cd84973844eafd3e47343
Rachit Shah <rachitshah@Rachits-MacBook-Pro.local>
2019-03-22 21:19:56 -0400

    port changes

--- CodeQualityPortal/CodeQualityPortal/runserver.py
+++ CodeQualityPortal/CodeQualityPortal/runserver.py
@@ -8,7 +8,7 @@ from CodeQualityPortal import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '8982'))
+        PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
-        PORT = 8982
+        PORT = 5555
     app.run(HOST, PORT)
